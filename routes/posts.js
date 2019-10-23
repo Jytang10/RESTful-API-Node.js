dc3d8f94ac59c59a7eb57b5b6195dad096cf2a9b
James Tang <jytang10@gmail.com>
2019-10-22 23:25:36 -0700

    Added update post method

--- routes/posts.js
+++ routes/posts.js
@@ -26,7 +26,7 @@ router.post('/', async (req,res) => {
   }
 });
 
-//Specific post
+//Find specific post
 router.get('/:postId', async (req,res) => {
   try{
   const post = await Post.findById(req.params.postId);
@@ -41,6 +41,18 @@ router.delete('/:postId', async (req,res) => {
   try{
   const removedPost = await Post.remove({_id: req.params.postId})
   res.json(removedPost);
+  }catch(err){
+    res.json({message:err})
+  }
+});
+
+//Update a post
+router.patch('/:postId', async (req,res) => {
+  try{
+  const updatedPost = await Post.updateOne(
+    { _id: req.params.postId },
+    { $set: { title: req.body.title } });
+  res.json(updatedPost);
   }catch(err){
     res.json({message:err})
   }
