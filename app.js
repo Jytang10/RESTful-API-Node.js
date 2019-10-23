4b431975d22e5fbd58bb73f5412c50d7d0565d15
James Tang <jytang10@gmail.com>
2019-10-22 22:22:40 -0700

    Setup dotenv and db connection

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express')
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 //ROUTES
 app.get('/', (req, res) => {
   res.send('We are on home');
 })
+
+app.get('/posts', (req, res) => {
+  res.send('We are on posts');
+})
+
+//Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  () => 
+  console.log('Connected to DB!')
+);
 
 //Start server
 app.listen(3000);
